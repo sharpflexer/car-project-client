417ae6cf4097ae3fb8678ca8a7cf9e6387334fdc
c.sharp.proger <c.sharp.proger@outlook.com>
2023-11-29 10:40:38 +0300

    Создал проект

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = "123";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {a}
         </p>
         <a
           className="App-link"
